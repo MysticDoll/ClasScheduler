5bb119029238880e526b452b4d99d4d437d42a4d
Mitsuru Takigahira <mysticdoll@mysticdoll.com>
2017-05-19 16:31:56 +0900

    Fix login function by reading csrf token

--- login/login.js
+++ login/login.js
@@ -2,11 +2,17 @@ const params = require("./loginparam");
 const req = require("../common/req.js");
 const path = params.path;
 const formdata = params.formdata;
+const jsdom = require("../common/jsdomPromise");
 var cookie;
 
 module.exports = req.get(path).then(function(res) {
   cookie = res.headers["set-cookie"][0].match(/JSESSIONID\=.+\;/)[0];
-  return req.post(path, formdata, cookie);
+  return jsdom(res.body)
+}).then(function(window) {
+  var param = {
+    "com.sun.faces.VIEW": window.document.getElementById("com.sun.faces.VIEW").getAttribute("value")
+  };
+  return req.post(path, Object.assign(param, formdata), cookie);
 }).then(function(res) {
   const body = res.body;
   if(body.match("<title>ポータル</title>")) {
